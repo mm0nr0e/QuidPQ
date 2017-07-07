ee4dc0f2ebbe7d3abc84ce5e4f9539f4c6917f4d
mm0nr0e <monroe.mirand@gmail.com>
2017-07-07 15:38:59 -0700

    added routes

--- server/server.js
+++ server/server.js
@@ -15,23 +15,35 @@ app.get('/', (req, res) => {
   // render homepage
 });                    
 
-app.get('/login', (req, res) => {
-  // render homepage
-});
-
 app.post('/login', (req, res) => {
-  // render homepage
+  // check if valid user and password
+  // if :) render user's collection page
 });
 
 app.get('/signup', (req, res) => {
-  // render homepage
+  // render signup page
 });
 
 app.post('/signup', (req, res) => {
-  // render homepage
+  // checks if form info was :)
+  // if so, make a new user document in db
 });
 
+app.get('/collection/:user', (req, res) => {
+  // check if valid user - if not redirect to /
+  // if valid, render user page with that users stuff from db
+});
 
+app.post('/collection/:user', (req, res) => {
+  // check if valid user on own page - if not redirect to /
+  // if valid, render user page with that users stuff from db
+});
+
+app.post('/messages/:user', (req, res) => {
+  // check if user is a valid user
+  // check if a different user is signed in
+  // if both :) , add a message to db with to: and from: props
+});
 
 app.listen(3000);
 
