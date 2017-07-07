d7190253768a9419229f37f4bead1f0fce58bcc2
mm0nr0e <monroe.mirand@gmail.com>
2017-07-07 16:43:25 -0700

    added middleware calls to routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+
+const userController = require('./../user/userController');
+const itemController = require('./../item/itemController');
+const messageController = require('./../message/messageController');
+const sessionController = require('./../session/sessionController');
+const cookieController = require('./../util/cookieController');
 
 const app = express();
 
@@ -9,37 +16,40 @@ const mongoURI = 'mongodb://localhost/quidPq';
 mongoose.connect(mongoURI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 // app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   // render homepage
 });                    
 
-app.post('/login', (req, res) => {
-  // check if valid user and password
-  // if :) render user's collection page
+app.post('/login', userController.verifyUser, cookieController.setSSIDCookie, sessionController.startSession, (req, res) => {
+  
 });
 
 app.get('/signup', (req, res) => {
-  // render signup page
+  res.sendFile(path.join(__dirname, './../public/sign-up.html'))
 });
 
-app.post('/signup', (req, res) => {
-  // checks if form info was :)
-  // if so, make a new user document in db
+app.post('/signup', userController.createUser, cookieController.setSSIDCookie, sessionController.startSession, (req, res) => {
+
 });
 
-app.get('/collection/:user', (req, res) => {
-  // check if valid user - if not redirect to /
-  // if valid, render user page with that users stuff from db
+app.get('/collection/:user', userController.isLoggedIn, itemController.getUserItems, messageController.getMessages, userController.isPageUser, (req, res) => {
+
 });
 
-app.post('/collection/:user', (req, res) => {
+app.get('/mycollection/:user', (req, res) => {
+
+});
+
+app.post('/mycollection/:user', itemController.addNewItem, (req, res) => {
   // check if valid user on own page - if not redirect to /
   // if valid, render user page with that users stuff from db
 });
 
-app.post('/messages/:user', (req, res) => {
+app.post('/messages/:user', messageController.addMessages, (req, res) => {
   // check if user is a valid user
   // check if a different user is signed in
   // if both :) , add a message to db with to: and from: props
